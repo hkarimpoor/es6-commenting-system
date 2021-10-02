ec797cf061b37e5594ea3acac4d347dd8f8753de
Hamid Karimpoor <hd.karimpoor@gmail.com>
2021-10-02 01:23:41 -0400

    basic js templating

--- app.js
+++ app.js
@@ -1,9 +1,37 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express'); 
+const port = 3000;
+var path = require('path');
+
+var app = module.exports = express();
+
+// Register ejs as .html. If we did
+// not call this, we would need to
+// name our views foo.ejs instead
+// of foo.html. The __express method
+// is simply a function that engines
+// use to hook into the Express view
+// system by default, so if we want
+// to change "foo.ejs" to "foo.html"
+// we simply pass _any_ function, in this
+// case `ejs.__express`.
+
+app.engine('.html', require('ejs').__express);
+
+// Optional since express defaults to CWD/views
+
+app.set('views', path.join(__dirname, 'views'));
+
+// Path to our public directory
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Without this you would need to
+// supply the extension to res.render()
+// ex: res.render('users.html').
+app.set('view engine', 'html');
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.render('home', { });
 })
 
 app.listen(port, () => {
